edae4ed8c42f54a4db78291a921f36de4aae27d7
Alexander Bij <AlexanderBij@godatadriven.com>
2020-03-31 22:52:21 +0200

    Tryout releasing through github-actions

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 setup(name='az-account-switcher',
-      version='0.0.2',
+      version='0.0.3',
       description='Utility to switch Azure subscriptions',
       long_description=long_description,
       long_description_content_type='text/markdown',
