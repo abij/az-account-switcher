ad5f767bf965548eb112eba8a5bf542ac8182ae6
Alexander Bij <AlexanderBij@godatadriven.com>
2021-05-28 21:59:50 +0200

    Try to add tests folder if run location is 1 level up.

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,12 @@ def azure_profile_dir(tmpdir):
     tmp_profile_dir = tmpdir.mkdir(".azure")
     os.environ['AZURE_CONFIG_DIR'] = str(tmp_profile_dir)
 
+    cwd = Path.cwd()
+
+    # Huu? are we not running inside the project folder?
+    if cwd.name == 'az-account-switcher':
+        cwd = cwd / 'tests'
+
     fresh_profile = Path.cwd() / '.azure/test-azureProfile.json'
 
     shutil.copy(fresh_profile, tmp_profile_dir / 'azureProfile.json')
